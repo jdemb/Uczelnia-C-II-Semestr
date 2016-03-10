320e44fba226ba4aea8d5ec0f9d94b97854f82d8
jentszej <jentszej@gmail.com>
2016-03-10 13:06:40 +0100

    Update zad1.c

--- lab2/zad1.c
+++ lab2/zad1.c
@@ -1,10 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void na_binarne(int z)
+{
+  int tab[8];
+  int i=0;
+  while(x!=0)
+  {
+    tab[i]=x%2;
+    x=x/2;
+    i++;
+  }
+  while(i>0)
+  {
+    i--;
+    printf("%i", tab[i]);
+  }
+  printf("\n");
+}
 int main()
 {
-  
-
-
-
-return 0;
+  char z;
+  z=getchar();
+  while(z!=EOF)
+  {
+    if(z!='\n') dec_bin(z);
+    z=getchar();
+  }
+  return 0;
 }
