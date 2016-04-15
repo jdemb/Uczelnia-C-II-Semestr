5a63bc48ae7f2c6f664c530d990e911b4e65df1d
Jedrzej Dembowski <jentszej@gmail.com>
2016-04-15 10:38:39 +0200

    upzad2b

---
+++ lab5/zad1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void iloraz_reszta(int a, int b,int *q, int *r);
+{
+    *q = a/b;
+    *r = a%b;
+}
+
+int main()
+{
+	int n, k, r, q;
+	printf("Podaj liczbe calkowita 'n' : ");
+	scanf("%i", &n);
+	printf("Podaj liczbe calkowita 'k', k=/=0 : ");
+	scanf("%i", &k);
+	iloraz_reszta(&q, &r, n, k);
+	printf("Iloraz calkowity : %i\n", q);
+	printf("Reszta z dzielenia : %i\n", r);
+	return 0;
+}
+
+
