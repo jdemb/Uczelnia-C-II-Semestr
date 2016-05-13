86553b4fa5ad8a60b9359bbd96281e9a84c8ed78
jentszej <jentszej@gmail.com>
2016-05-13 11:21:43 +0200

    Update zad4.c

--- Lab8/zad4.c
+++ Lab8/zad4.c
@@ -1,10 +1,10 @@
-// Napisaæ makrodefinicjê PETLA, której wywo³anie
+// NapisaÄ‡ makrodefinicjÄ™ PETLA, ktÃ³rej wywoÅ‚anie
 // PETLA(komenda1, warunek, komenda2)
-// dzia³a tak, jak na schemacie obok (pêtla while ,,zroœniêta'' z pêtl¹ do...while).
-// Na przyk³ad wywo³anie:
+// dziaÅ‚a tak, jak na schemacie obok (pÄ™tla while ,,zroÅ›niÄ™ta'' z pÄ™tlÄ… do...while).
+// Na przykÅ‚ad wywoÅ‚anie:
 // a=0; b=0; i=0;
 // PETLA(a++, i<5, b++; i++);
-// powinno przypisywaæ zmiennej a wartoœæ 6, a zmiennej b wartoœæ 5.
+// powinno przypisywaÄ‡ zmiennej a wartoÅ›Ä‡ 6, a zmiennej b wartoÅ›Ä‡ 5.
 
 #include <stdio.h>
 #define PETLA(komenda1, warunek, komenda2) komenda1; while(warunek){komenda2; komenda1;}
@@ -14,5 +14,4 @@ int main()
   PETLA(a++, i<5, b++; i++);
   printf("%i\n", a);
   printf("%i\n", b);
-  return 0;
 }
