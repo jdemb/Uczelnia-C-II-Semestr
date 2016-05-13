ecb9f65fc87ed8e8119e6394b5418433393abedd
jentszej <jentszej@gmail.com>
2016-05-13 11:08:11 +0200

    Update zad2.c

--- Lab8/zad2.c
+++ Lab8/zad2.c
@@ -1,12 +1,12 @@
 
-/*Napisaæ odpowiednie makrodefinicje i ich wywo³ania, ¿eby zosta³ wygenerowany tekst
+/*NapisaÄ‡ odpowiednie makrodefinicje i ich wywoÅ‚ania, Å¼eby zostaÅ‚ wygenerowany tekst
  	Najwyzszym szczytem Azji jest Mt. Everest.
  	Najwyzszym szczytem Europy jest Elbrus.
  	Najwyzszym szczytem Afryki jest Kilimandzaro.
  	Najwyzszym szczytem Australii jest Gora Kosciuszki.
-i ¿eby nie powtarzaæ tych samych jego s³ów wielokrotnie. */
+i Å¼eby nie powtarzaÄ‡ tych samych jego sÅ‚Ã³w wielokrotnie. */
 
-#define INFO(kraj, szczyt) Najwyzszym szczytem kraj jest szczyt.
+#define INFO(kraj, szczyt) Najwyzszym szczytem #kraj jest #szczyt.
 INFO(Azji, Mt. Everest)
 INFO(Europy, Elbrus)
 INFO(Afryki, Kilimandzaro)
